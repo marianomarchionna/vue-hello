c5c235c71f85bbdd2623ad4c4d5721a0e260b491
Mariano <marianomarchionna8@gmail.com>
2021-09-06 15:38:34 +0200

    Stampa con i data

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: 'Hello world by me'
+        }
+    }
+)
